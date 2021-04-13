e6e0a7d3ffb707b686cbeb0fada52b4b7d257b8f
Youssef baddi <baddiyoussef85@gmail.com>
2021-04-13 09:09:35 +0000

    add correction exercice 1

---
+++ TP3/exercice1/vecteur3d.h
@@ -0,0 +1,17 @@
+//
+// Created by baddi on 13/4/2021.
+//
+
+#ifndef UNTITLED1_VECTEUR3D_H
+#define UNTITLED1_VECTEUR3D_H
+
+class vecteur3d {
+    float x,y,z;
+public:
+    vecteur3d();
+    vecteur3d(float,float,float);
+};
+
+
+
+#endif //UNTITLED1_VECTEUR3D_H
---
+++ TP3/exercice1/vecteur3d_2.h
@@ -0,0 +1,18 @@
+//
+// Created by baddi on 13/4/2021.
+//
+
+#ifndef UNTITLED1_VECTEUR3D_H
+#define UNTITLED1_VECTEUR3D_H
+
+class vecteur3d {
+    float x,y,z;
+public:
+
+    vecteur3d(float x1=0,float y1=0,float z1=0): x(x1), y(y1), z(z1){
+    }
+};
+
+
+
+#endif //UNTITLED1_VECTEUR3D_H
